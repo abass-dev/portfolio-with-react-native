bbf2089c9684e2be8389c61a761cb7d2880ab85a
Abass Ben Cheick <cheikabassben@gmail.com>
2022-02-19 01:10:15 +0000

    up(comp): update Project

--- comp/Project.js
+++ comp/Project.js
@@ -11,28 +11,27 @@ class Project extends Component {
     render() {
         return(
             <SafeAreaView style={style.defaultContainer}>
-	            <Text style={{fontSize:30, color: '#fff', marginVertical: 15}}>RECENT PROJECTS</Text>
-	            <ScrollView showsVerticalScrollIndicator={false}>
-                    <Dynacard
-                        image={require("../assets/images/php_code.jpg")}
-                        title="NIGATEDEV FRAMEWORK"
-                        headLinks={[
-                            {link: 'https://github.com/nigatedev/nigatedev', name: 'Github'},
-                            {link: 'https://packagist.org/packages/nigatedev/nigatedev', name: 'Packagist'},
-                            {link: 'https://github.com/nigatedev/nigatedev/issues', name: 'Issues'}
-                        ]}
-                        lansUsed="PHP, JavaScript, Sass, Shell, Twig"
-                        description="Nigatedev is a time-saving PHP framework coded by Abass Ben Cheik and contributors."
-                        btnGo={{title: 'The code source', link: 'https://github.com/nigatedev'}}
-                    />
-                    <Dynacard
-                        image={require("../assets/images/php_code.jpg")}
-                        title="TODAYDEVS (WebSite)"
-                        lansUsed="WordPress"
-                        description="With TodayDevs, stay informed about new technology! We share everything about new technologies concerning web, mobile and software while trying to stay up to date with new technologies."
-                        btnGo={{title: 'The WebSite', link: 'https://todaydevs.com'}}
-                    />
-	            </ScrollView>
+                    <ScrollView showsVerticalScrollIndicator={false}>
+                        <Dynacard
+                            image={require("../assets/images/php_code.jpg")}
+                            title="NIGATEDEV FRAMEWORK"
+                            headLinks={[
+                                {link: 'https://github.com/nigatedev/nigatedev', name: 'Github'},
+                                {link: 'https://packagist.org/packages/nigatedev/nigatedev', name: 'Packagist'},
+                                {link: 'https://github.com/nigatedev/nigatedev/issues', name: 'Issues'}
+                            ]}
+                            lansUsed="PHP, JavaScript, Sass, Shell, Twig"
+                            description="Nigatedev is a time-saving PHP framework coded by Abass Ben Cheik and contributors."
+                            btnGo={{title: 'The code source', link: 'https://github.com/nigatedev'}}
+                        />
+                        <Dynacard
+                            image={require("../assets/images/php_code.jpg")}
+                            title="TODAYDEVS (WebSite)"
+                            lansUsed="WordPress"
+                            description="With TodayDevs, stay informed about new technology! We share everything about new technologies concerning web, mobile and software while trying to stay up to date with new technologies."
+                            btnGo={{title: 'The WebSite', link: 'https://todaydevs.com'}}
+                        />
+    	            </ScrollView>
 	        </SafeAreaView>
         )
     }
